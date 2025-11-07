4562d5f53e49e011d398969ebd5556ffc9446d50
oscardbpucp <oscarpucp@gmail.com>
2025-11-06 22:59:53 -0500

    Clase 12 - Introducción a los microservicios - EurekaClient Hello

---
+++ src/test/java/com/example/eurekaclient_clase12/ClientEurekaClase12ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eurekaclient_clase12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientEurekaClase12ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
